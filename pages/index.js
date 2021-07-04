bb0ab6c3fa92416377802e6303206f9ba5e4c1ab
Kazuumi Nishimura <kazuumi@zoho.com>
2021-07-04 09:23:29 +0900

    test branch init

--- pages/index.js
+++ pages/index.js
@@ -25,7 +25,7 @@ export default function Home({allPostsData}) {
         <p>Sophomore at Department of Ecoregion Science, Tokyo University of Agriculture and Technology.</p>
         <p>
           (This is a sample website - you'll be building a site like this on{' '})
-          <a href="https://nextjs.org/learn">our Next.js tutorial</a>.)
+          <a href="https://nextjs.org/learn">our Next.js tutorial</a>.
         </p>
       </section>
       <section className={`${utilStyles.headingMd} ${utilStyles.padding1px}`}>
